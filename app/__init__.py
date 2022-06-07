d4cc151f23e5c1174eb119f5097d19fc2a5742c1
Sebastian Granda Gallego <sgg10.develop@gmail.com>
2022-06-06 21:51:31 -0500

    Files

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,16 @@ from typing import Optional
 # FastAPI
 from fastapi import FastAPI
 from fastapi import status
-from fastapi import Body, Path, Query, Form, Header, Cookie
+from fastapi import (
+    Body,
+    Path,
+    Query,
+    Form,
+    Header,
+    Cookie,
+    File,
+    UploadFile
+)
 
 # Starlette
 from starlette.responses import RedirectResponse
@@ -124,3 +133,13 @@ def contact(
     ads: Optional[str] = Cookie(default=None)
 ):
     return user_agent
+
+@app.post("/post-image")
+def post_image(
+    image: UploadFile = File(...)
+):
+    return {
+        "Filename": image.filename,
+        "Format": image.content_type,
+        "Size(kb)": round(len(image.file.read())/1024, 2)
+    }
